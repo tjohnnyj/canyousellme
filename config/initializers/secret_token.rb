5ae5fe07320cf9118bb199fd119eb72e2ddd9193
trace.johnson@livingsocial.com <trace.johnson@livingsocial.com>
2014-05-17 14:49:29 -0400

    using 3.2.18

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canyousellme::Application.config.secret_token = '5f987d857d2cb0c2f61ba6d39cf3e8b1a7a32306ba009f6e3f7af12c0ef23adc72fb0a96b862b1050b95f437a30f40695d3bc20febe747e1375c0a064ee60549'
